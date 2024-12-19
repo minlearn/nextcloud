f7eb18442a40cb553d93f88b0aa220b2465ff9dc
minlearnminlearn <minlearn@minlearn.org>
2024-12-20 00:36:22 +0800

    Update nextcloud_install.sh

--- nextcloud_install.sh
+++ nextcloud_install.sh
@@ -1,32 +1,31 @@
 ###############
 
+silent() { "$@" >/dev/null 2>&1; }
+
 echo "Installing Dependencies"
-apt-get install -y curl
-apt-get install -y sudo
-apt-get install -y mc
+silent apt-get install -y curl sudo mc
 echo "Installed Dependencies"
 
 NEXTCLOUD_DIR=/var/www/nextcloud
 
 # Install Apache, PHP, and necessary PHP extensions
 echo "Installing Apache and PHP..."
-sudo apt install apache2 libapache2-mod-php php-gd php-json php-sqlite3 php-mysql php-curl php-mbstring \
-                 php-intl php-imagick php-xml php-zip -y
+silent apt install apache2 libapache2-mod-php php-gd php-json php-sqlite3 php-mysql php-curl php-mbstring php-intl php-imagick php-xml php-zip -y
 
 # Enable Apache mods
-sudo a2enmod rewrite headers env dir mime
+a2enmod rewrite headers env dir mime
 
 
 
 # Install Nextcloud
 echo "Installing Nextcloud..."
-sudo mkdir -p ${NEXTCLOUD_DIR}
+mkdir -p ${NEXTCLOUD_DIR}
 cd ${NEXTCLOUD_DIR}/..
 # php7.4 supports most to nextcloud-25
-sudo wget https://download.nextcloud.com/server/releases/latest-25.tar.bz2 -O latest.tar.bz2
-sudo tar -xjf latest.tar.bz2 -C ${NEXTCLOUD_DIR} --strip-components=1
-sudo rm latest.tar.bz2
-sudo chown -R www-data:www-data ${NEXTCLOUD_DIR}
+wget https://download.nextcloud.com/server/releases/latest-25.tar.bz2 -O latest.tar.bz2
+tar -xjf latest.tar.bz2 -C ${NEXTCLOUD_DIR} --strip-components=1
+rm latest.tar.bz2
+chown -R www-data:www-data ${NEXTCLOUD_DIR}
 
 # Configure Apache to serve Nextcloud
 echo "Configuring Apache..."
@@ -43,18 +42,18 @@ echo "<VirtualHost *:80>
             Dav off
         </IfModule>
     </Directory>
-</VirtualHost>" | sudo tee $NEXTCLOUD_CONF
+</VirtualHost>" | tee $NEXTCLOUD_CONF
 
-sudo a2ensite nextcloud.conf
-sudo a2dissite 000-default.conf
-sudo systemctl restart apache2
+a2ensite nextcloud.conf
+a2dissite 000-default.conf
+systemctl restart apache2
 
 echo "Nextcloud installation completed successfully!"
 echo "You can access Nextcloud at: http://${DOMAIN_OR_IP}/"
 
 echo "Cleaning up"
-apt-get -y autoremove
-apt-get -y autoclean
+silent apt-get -y autoremove
+silent apt-get -y autoclean
 echo "Cleaned"
 
 ##############
